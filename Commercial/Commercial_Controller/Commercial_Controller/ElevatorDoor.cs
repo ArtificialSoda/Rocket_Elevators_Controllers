c88c331fc32262efea4cee923a9ab35c47b2bc2b
Fabien H. Dimitrov <dimitrov.fabien@gmail.com>
2020-10-05 10:04:07 -0400

    Initial commit (C#)

---
+++ Commercial/Commercial_Controller/Commercial_Controller/ElevatorDoor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commercial_Controller
+{
+    class ElevatorDoor
+    {
+    }
+}
---
+++ Commercial/Commercial_Controller/Commercial_Controller/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Commercial_Controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
