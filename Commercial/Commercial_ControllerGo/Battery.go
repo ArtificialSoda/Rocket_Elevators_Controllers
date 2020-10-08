6ea014f7fa01a865c1a143d2566dd7f7cfe9b77b
Fabien H. Dimitrov <dimitrov.fabien@gmail.com>
2020-10-08 15:04:10 -0400

    Update Commercial Controller in Go

--- Commercial/Commercial_ControllerGo/Battery.go
+++ Commercial/Commercial_ControllerGo/Battery.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // STATIC PROPERTIES (in Golang, it's treated as a global)
 var NumFloors int
 var NumBasements int
@@ -8,10 +13,13 @@ var NumColumns int
 type Battery struct {
 
 	// PROPERTIES
-	ID              int
-	Status          string // online|offline
-	ColumnList      []Column
-	BoardButtonList []BoardButton
+	ID                  int
+	Status              string // online|offline
+	ColumnList          []Column
+	BoardButtonList     []BoardButton
+	isFire              bool
+	isPowerOutage       bool
+	isMechanicalFailure bool
 }
 
 // METHODS
@@ -32,5 +40,78 @@ func (battery *Battery) CreateBoardButtons() {
 		button := BoardButton{}
 		button.InitBoardButton(numFloor, battery)
 		battery.BoardButtonList = append(battery.BoardButtonList, button)
+	}
+}
+
+// Monitor the battery's elevator system
+// In real conditions, the monitoring would be done in a near-infinite while loop
+func (battery *Battery) MonitorSystem() {
+
+	shouldExit := false
+
+	if battery.isFire || battery.isPowerOutage || battery.isMechanicalFailure {
+
+		battery.Status = "offline"
+
+		for i := range battery.ColumnList {
+
+			battery.ColumnList[i].Status = "offline"
+			for j := range battery.ColumnList[i].ElevatorList {
+
+				battery.ColumnList[i].ElevatorList[j].Status = "offline"
+			}
+		}
+		fmt.Printf("Battery %d has been shut down for maintenance. Sorry for the inconvenience.", battery.ID)
+		shouldExit = true
+	}
+
+	// Stop execution of script if something is wrong
+	if shouldExit {
+		os.Exit(-1)
+	}
+}
+
+// Assigns a column and elevator to use when making a request from RC via the board buttons
+func (battery *Battery) AssignElevator(requestedFloor int) {
+
+	// Select the call button to press
+	var boardBtnToPress BoardButton
+	for i := range battery.BoardButtonList {
+
+		if battery.BoardButtonList[i].RequestedFloor == requestedFloor {
+			boardBtnToPress = battery.BoardButtonList[i]
+			break
+		}
+	}
+
+	// Select the chosen elevator
+	var chosenElevator Elevator
+	if elevator, err := boardBtnToPress.Press(); err != nil {
+		chosenElevator = elevator
+
+	} else {
+		fmt.Println("AssignElevator(), Battery.go FAILED: ", err)
+	}
+
+	// Do requests until elevator has reached the floor where the call was made (RC floor)
+	for chosenElevator.CurrentFloor != OriginFloor {
+		chosenElevator.DoRequests()
+	}
+
+	// Set a request for the elevator to go to requested floor, once picked up
+	newDirection := ""
+	if chosenElevator.CurrentFloor < requestedFloor {
+		newDirection = "up"
+
+	} else {
+		newDirection = "down"
+	}
+
+	r := Request{requestedFloor, newDirection}
+	chosenElevator.RequestsQueue = append(chosenElevator.RequestsQueue, r)
+
+	// Do requests until elevator has reached requested floor
+	for chosenElevator.CurrentFloor != requestedFloor {
+		chosenElevator.DoRequests()
 	}
 }
--- Commercial/Commercial_ControllerGo/Column.go
+++ Commercial/Commercial_ControllerGo/Column.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // STATIC PROPERTIES (in Golang, it's treated as a global)
 var NumElevators int = 1
 
@@ -23,5 +25,44 @@ func (column *Column) CreateElevatorList() {
 		elevator := Elevator{}
 		elevator.InitElevator(elevatorID, column)
 		column.ElevatorList = append(column.ElevatorList, elevator)
+	}
+}
+
+// Initialize all the call buttons, on each floor
+func (column *Column) CreateCallButtons() {
+
+	for numFloor := column.LowestFloor; numFloor <= column.HighestFloor; numFloor++ {
+
+		button := CallButton{}
+		button.InitCallButton(numFloor, column)
+		column.CallButtonList = append(column.CallButtonList, button)
+	}
+}
+
+// Request an elevator to current location (if current location is not RC)
+func (column *Column) RequestElevator(floorNumber int) {
+
+	// Select the call button to press
+	var callBtnToPress CallButton
+	for i := range column.CallButtonList {
+
+		if column.CallButtonList[i].Floor == floorNumber {
+			callBtnToPress = column.CallButtonList[i]
+			break
+		}
+	}
+
+	// Select the chosen elevator
+	var chosenElevator Elevator
+	if elevator, err := callBtnToPress.Press(); err != nil {
+		chosenElevator = elevator
+
+	} else {
+		fmt.Println("RequestElevator(), Column.go FAILED: ", err)
+	}
+
+	// Do requests until elevator has reached the final destination (RC)
+	for chosenElevator.CurrentFloor != OriginFloor {
+		chosenElevator.DoRequests()
 	}
 }
